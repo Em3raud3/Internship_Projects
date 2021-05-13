fbd40e06f99980594f836fdba49274684f3da7a1
anime <anime@DESKTOP-5EJ9AMC.lan>
2021-05-13 14:46:10 -0400

    CSV works perfectly, as well as FLASK. Just need to edit the html file

--- AccessLog.py
+++ AccessLog.py
@@ -1,74 +1,77 @@
 from pathlib import Path
 from datetime import datetime
 import csv
+from flask import Flask, render_template
+app = Flask(__name__)
 
+
+@app.route('/')
+def loadAccessFile():
+    logList = []
+    localList = []
+    uniqueID = set()
+    uniqueDate = set()
+    uniqueName = set()
     
-logList = []
-localList = []
-uniqueID = set()
-uniqueDate = set()
-uniqueName = set()
-
-fr = open("AccessLog.txt", "rt")
-
-for x in fr:
-    tempList = x.split()
-    logList.append([tempList[2], ((tempList[3])[1:]), tempList[8], tempList[2] + ((tempList[3])[1:])])
-    uniqueID.add(tempList[2] + ((tempList[3])[1:]))
-    uniqueDate.add((tempList[3])[1:])
-    uniqueName.add(tempList[2])
-
-fr.close()   
-
-#Sort the Set
-uniqueDate = list(uniqueDate)
-uniqueDate.sort(key = lambda date: datetime.strptime(date, '%d/%b/%Y'))
-uniqueName = sorted(uniqueName)
-
-
-for x in uniqueID:
-    length = len(x)
-    loginCount = 0
-    name = x[0:length + -11]
-    date = x[length + -11:]
-
-    for y in logList:
-        if y[3] == x and y[2] == "200":
-            loginCount += 1
+    fr = open("AccessLog.txt", "rt")
     
-    localList.append([name, date, loginCount])
+    for x in fr:
+        tempList = x.split()
+        logList.append([tempList[2], ((tempList[3])[1:]), tempList[8], tempList[2] + ((tempList[3])[1:])])
+        uniqueID.add(tempList[2] + ((tempList[3])[1:]))
+        uniqueDate.add((tempList[3])[1:])
+        uniqueName.add(tempList[2])
     
+    fr.close()   
     
-finalList = []
-
-
-for date in uniqueDate:
-    tempList = [0] * 85 
-    tempList[0] = date
-    finalList.append(tempList)
+    uniqueDate = list(uniqueDate)
+    uniqueDate.sort(key = lambda date: datetime.strptime(date, '%d/%b/%Y'))
+    uniqueName = sorted(uniqueName)
     
-for record in localList:
-    row = uniqueDate.index(record[1])
-    col = uniqueName.index(record[0]) + 1
-    finalList[row][col] = record[2]
-
-
+    for x in uniqueID:
+        length = len(x)
+        loginCount = 0
+        name = x[0:length + -11]
+        date = x[length + -11:]
         
-Database = Path("static/js/Database.csv")
-Database.touch(exist_ok=True)
-with open("static/js/Database.csv", 'w', newline='') as Database:
-    dataWriter = csv.writer(Database)
+        for y in logList:
+            if y[3] == x and y[2] == "200":
+                loginCount += 1
+                
+        localList.append([name, date, loginCount])
+        
+    finalList = []
     
-    csvHeader = []
-    csvHeader.append("Date")
+    for date in uniqueDate:
+        tempList = [0] * 85 
+        tempList[0] = date
+        finalList.append(tempList)
+        
+    for record in localList:
+        row = uniqueDate.index(record[1])
+        col = uniqueName.index(record[0]) + 1
+        finalList[row][col] = record[2]
+
+    Database = Path("static/js/Database.csv")
+    Database.touch(exist_ok=True)
+    with open("static/js/Database.csv", 'w', newline='') as Database:
+        dataWriter = csv.writer(Database)
+        
+        csvHeader = []
+        csvHeader.append("Date")
+        
+        for x in uniqueName:
+            csvHeader.append(x)
+            
+        dataWriter.writerow(csvHeader)
+        
+        for x in finalList:
+            dataWriter.writerow(x)
     
-    for x in uniqueName:
-        csvHeader.append(x)
     
-    dataWriter.writerow(csvHeader)
+    return render_template("index.html")
     
-    for x in finalList:
-        dataWriter.writerow(x)
+
  
     
 #print(finalList)
