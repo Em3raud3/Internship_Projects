a796f9acc2467b4bde8d156f869583846bd3fcec
anime <anime@DESKTOP-5EJ9AMC.lan>
2021-05-13 14:11:37 -0400

    just for safety

--- AccessLog.py
+++ AccessLog.py
@@ -4,15 +4,15 @@ import csv
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def loadAccessFile():
     
     logList = []
+    localList = []
     uniqueID = set()
     uniqueDate = set()
     uniqueName = set()
-    
+        
     fr = open("AccessLog.txt", "rt")
     
     for x in fr:
@@ -24,28 +24,65 @@ def loadAccessFile():
     
     fr.close()   
     
+    #Sort the Set
+    uniqueDate = list(uniqueDate)
+    uniqueDate.sort(key = lambda date: datetime.strptime(date, '%d/%b/%Y'))
+    uniqueName = sorted(uniqueName)
+    
+    
+    for x in uniqueID:
+        length = len(x)
+        loginCount = 0
+        name = x[0:length + -11]
+        date = x[length + -11:]
+    
+    for y in logList:
+        if y[3] == x and y[2] == "200":
+            loginCount += 1
+    
+    localList.append([name, date, loginCount])
+    
+    
+    finalList = []
+    
+    
+    for date in uniqueDate:
+        tempList = [0] * 85 
+        tempList[0] = date
+        finalList.append(tempList)
+    
+    for record in localList:
+        row = uniqueDate.index(record[1])
+        col = uniqueName.index(record[0]) + 1
+        finalList[row][col] = record[2]
+    
+    
+        
     Database = Path("static/js/Database.csv")
     Database.touch(exist_ok=True)
     
     with open("static/js/Database.csv", 'w', newline='') as Database:
         dataWriter = csv.writer(Database)
-        
-        dataWriter.writerow(["UserID","Date", "Daily_Logins"])
-        
-        for x in uniqueID:
-            length = len(x)
-            loginCount = 0
-            name = x[0:length + -11]
-            date = x[length + -11:]
-        
-            for y in logList:
-                if y[3] == x and y[2] == "200":
-                    loginCount += 1
-            
-            dataWriter.writerow([name, date, loginCount]) 
     
-    return render_template("index.html")
+    csvHeader = []
+    csvHeader.append("Date")
+    
+    for x in uniqueName:
+        csvHeader.append(x)
+    
+    dataWriter.writerow(csvHeader)
+    
+    for x in finalList:
+        dataWriter.writerow(x)
 
+    return render_template("index.html")
+            
+    
+#print(finalList)
+#print(localList)
+#print(len(uniqueName))
+# print(len(uniqueDate))
+    
 #for x in logList:
     #strA = str(x[0])
     #strB = str(x[1])
