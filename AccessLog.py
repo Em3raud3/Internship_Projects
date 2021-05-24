f5cf034e8438c95996e1ee5b9046b8764fd755cf
Jeffrey Weng <61710685+Em3raud3@users.noreply.github.com>
2021-05-24 10:35:11 -0400

    Updated to final version

--- AccessLog.py
+++ AccessLog.py
@@ -12,41 +12,41 @@ def loadAccessFile():
     uniqueID = set()
     uniqueDate = set()
     uniqueName = set()
-    
+
     fr = open("AccessLog.txt", "rt")
-    
+
     for x in fr:
         tempList = x.split()
         logList.append([tempList[2], ((tempList[3])[1:]), tempList[8], tempList[2] + ((tempList[3])[1:])])
         uniqueID.add(tempList[2] + ((tempList[3])[1:]))
         uniqueDate.add((tempList[3])[1:])
         uniqueName.add(tempList[2])
-    
-    fr.close()   
-    
+
+    fr.close()
+
     uniqueDate = list(uniqueDate)
     uniqueDate.sort(key = lambda date: datetime.strptime(date, '%d/%b/%Y'))
     uniqueName = sorted(uniqueName)
-    
+
     for x in uniqueID:
         length = len(x)
         loginCount = 0
         name = x[0:length + -11]
         date = x[length + -11:]
-        
+
         for y in logList:
             if y[3] == x and y[2] == "200":
                 loginCount += 1
-                
+
         localList.append([name, date, loginCount])
-        
+
     finalList = []
-    
+
     for date in uniqueDate:
-        tempList = [0] * 85 
+        tempList = [0] * 85
         tempList[0] = date
         finalList.append(tempList)
-        
+
     for record in localList:
         row = uniqueDate.index(record[1])
         col = uniqueName.index(record[0]) + 1
@@ -56,30 +56,31 @@ def loadAccessFile():
     Database.touch(exist_ok=True)
     with open("static/js/Database.csv", 'w', newline='') as Database:
         dataWriter = csv.writer(Database)
-        
+
         csvHeader = []
         csvHeader.append("Date")
-        
+
         for x in uniqueName:
             csvHeader.append(x)
-            
+
         dataWriter.writerow(csvHeader)
-        
+
         for x in finalList:
             dataWriter.writerow(x)
-            
-    
-    
-    return render_template("index.html")
-    
 
- 
-    
+
+
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+
+
 #print(finalList)
 #print(localList)
 #print(len(uniqueName))
 # print(len(uniqueDate))
-    
+
 #for x in logList:
     #strA = str(x[0])
     #strB = str(x[1])
@@ -87,8 +88,8 @@ def loadAccessFile():
     #tempStr = strA + "," + strB + "," + strC
     #fw.write(tempStr)
     #fw.write("\n")
-    
+
 #print(uniqueID)
 #print(logList[0][2], (logList[0][3])[1:], logList[0][8])
 #print(logList)
-#print(f.read())            
+#print(f.read())
