7e69eb308927a4afefe2bd9271e9a74cc5c51d9c
anime <anime@host.docker.internal>
2021-06-04 09:41:58 -0400

    Added Necessary Directory back in

--- AccessLog.py
+++ AccessLog.py
@@ -1,8 +1,98 @@
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import csv
-from flask import Flask, render_template
+from flask import Flask, render_template , request
+import os
+import gzip
+import glob
+import time
+import sys
+
 app = Flask(__name__)
+
+@app.route('/Update')
+def createAccessFile():
+    #calculate time period to look back based on user preference 
+    #Checks for numbers of days to look back either from the sys.argv or api request
+    
+    daysToLookBack = request.args.get("Days")
+    
+    if daysToLookBack == None:
+        try:
+            daysToLookBack = int(sys.argv[1])
+        
+        except: 
+            daysToLookBack = 150
+            
+    else: daysToLookBack = int(daysToLookBack)
+    
+    todayDate = date.today()
+    deltaDay = timedelta(days=daysToLookBack)
+    
+    #This will be the earlist date we look back
+    earliestDate = todayDate - deltaDay
+    
+    #Listed all file names in directory in directory and sort by time
+    fileFolder = glob.glob("nginx/access.log.*")
+    fileFolder.sort(key=os.path.getmtime, reverse=True)
+    
+    #Will contain all data from all longs
+    totalData =""
+    
+    pathToFolder= "/nginx"
+    
+    #Iterate through files and add them to totalData
+    for fileName in fileFolder:
+    
+        print("Processing Log: " + fileName)
+        
+        fr = None
+    
+        if fileName[len(fileName) - 3:] == ".gz":
+            fr = gzip.open(fileName,"rt")  
+        
+        else:
+            fr = open(fileName, "r") 
+        
+        #Split the lines into arrays to choose what data we is desired, had to format a lot to get it in right form
+        for line in fr:
+            line = line.split()
+            tempDate = line[3]
+            tempDate = tempDate[:12] + " " + tempDate[13:] +"]"
+            line[3] = tempDate
+            selection =""
+            
+            if len(line[2]) > 1 and len(line[2]) < 21:
+                del line[4]
+                
+            
+            try:
+                if datetime.date(datetime.strptime(line[3][1:21], "%d/%b/%Y %H:%M:%S")) > earliestDate and line[7] =="200":
+                
+                    for items in line[0:10]:
+                        selection += items
+                        selection += " "
+            
+            
+                    if len(line[2]) > 1 and len(line[2]) < 21:
+                        totalData += selection + "\n"
+            
+            except:
+                continue
+        
+        fr.close()
+
+        
+    Database = Path("app.qcr.io.access.log")
+    Database.touch(exist_ok=True)
+    
+    with open("app.qcr.io.access.log", 'w') as Database:
+       # Database.write(Data)
+    
+       for line in totalData:
+           Database.write(line)
+
+    return render_template("Updated.html")
 
 
 @app.route('/')
